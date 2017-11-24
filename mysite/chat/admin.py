dd62df0efea12bcf963bfcdfea8e4de493fbc32e
Artem01011991 <31074309+Artem01011991@users.noreply.github.com>
2017-11-24 19:14:44 +0300

    moved from ITStep_Python

---
+++ mysite/chat/admin.py
@@ -0,0 +1,8 @@
+from django.contrib import admin
+from .models import UserAccount
+
+class UserAccountAdmin(admin.ModelAdmin):
+    list_display = ('user_photo',)
+
+
+admin.site.register(UserAccount, UserAccountAdmin)
